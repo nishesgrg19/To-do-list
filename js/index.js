e773d017447836aa0bd175253e652b0bad464dbc
nishes gurung <nishesgrg16@gmail.com>
2022-10-16 19:57:34 +0545

    documents of to do list

---
+++ js/index.js
@@ -0,0 +1,37 @@
+let add=document.getElementById('add')
+let listc=document.querySelector('#listc')
+add.addEventListener('click',getlist)
+
+function getlist(){
+
+    let screen=document.getElementById('screen')
+    let screendata=screen.value
+    let listdata=document.createElement('li')
+  
+    listdata.setAttribute('class','listvalue')
+    listdata.innerHTML=`<span class='ld'>${screendata}</span>`
+
+   
+    if (listdata.innerText!=''){
+        listc.appendChild(listdata)
+        screen.value=''
+
+   
+    }
+    let cross=document.createElement('span')
+    cross.setAttribute('class','cross')
+    listdata.appendChild(cross)
+   
+
+    
+    // deleting the contents//
+
+    let remove=document.querySelectorAll('.cross')
+    remove.forEach((item)=>{
+        item.addEventListener('click',()=>{
+            item.parentElement.remove()
+        })
+    })
+   
+   
+}
